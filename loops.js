fb8196b56823040aca292b2c921a4bc60b248841
Julie  Kim <julieskim160@gmail.com>
2017-01-24 23:14:30 +0000

    Done.

---
+++ loops.js
@@ -0,0 +1,26 @@
+function forLoop(array) {
+  for (var i = 1; i < 26; i++) {
+    sRet= `"I am ${i} strange loop${i===1 ? '' : 's'}."`
+    array.push(sRet);
+  }
+  return array
+}
+
+function whileLoop(n) {
+  while (n>0) {
+    console.log(--n)
+  }
+  return "done"
+}
+
+
+function maybeTrue(){
+  return Math.random() >= 0.5
+}
+
+function doWhileLoop(array){
+  do {
+      array.pop()
+  } while (array.length=0 && maybeTrue());
+  return array
+}
